ce80f35aaeea50ad6dfd7e6ca3dcdfc7662b7cb8
greekvirus1 <greekvirus1@hotmail.gr>
2024-06-27 20:36:48 +0100

    moved unused functions

--- custom_size_array_functions.py
+++ custom_size_array_functions.py
@@ -11,5 +11,5 @@ def custom_array_2d(x, y):
     returns 2d array of custom size
     :param x, y: int
     '''
-    arr = [[0] * x for _ in range(y)] #wtf is this? https://stackoverflow.com/questions/2739552/2d-list-has-weird-behavor-when-trying-to-modify-a-single-value
+    arr = [[0] * x for _ in range(y)] #wtf is this? - https://stackoverflow.com/questions/2739552/2d-list-has-weird-behavor-when-trying-to-modify-a-single-value
     return arr
