4821ff29d1c0d66094598a0ca9c621b311b60037
greekvirus1 <greekvirus1@hotmail.gr>
2024-07-15 21:14:17 +0100

    the best I could do
    Absolutely terrible

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,3 +1,5 @@
+import random # type: ignore
+
 def sudoku_checker(board):
     valid_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     #check rows
@@ -45,3 +47,72 @@ def sudoku_checker(board):
         else:
             x += 1
             check_this = []
+
+def sudoku_find_square(x,y):
+    '''
+    Returns a list of the coordinates where the 3x3 square associated of X and Y starts
+    '''
+    if x < 3:
+        if y < 3:
+            return [0, 0]
+        elif y < 6:
+            return [0, 3]
+        else:
+            return [0, 6]
+    elif x < 6:
+        if y < 3:
+            return [3, 0]
+        elif y < 6:
+            return [3, 3]
+        else:
+            return [3, 6]
+    else:
+        if y < 3:
+            return [6, 0]
+        elif y < 6:
+            return [6, 3]
+        else:
+            return [6, 6]
+
+def sudoku_solver(board):
+    board_copy = board.copy()
+    attempts = 1
+    x = 0
+    while True:
+        for x in range(9):
+            y = 0
+            for y in range(9):
+                print(x,y,attempts)
+                if board_copy[x][y] == 0:
+                    valid_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+                    #Check row
+                    for i in range(9):
+                        if board_copy[i][y] != 0:
+                            if board_copy[i][y] in valid_numbers:
+                                valid_numbers.remove(board_copy[i][y])
+                    #Check column
+                    for j in range(9):
+                        if board_copy[x][j] !=0:
+                            if board_copy[x][j] in valid_numbers:
+                                valid_numbers.remove(board_copy[x][j])
+                    #Check 3x3 square
+                    sqr_coordinates = sudoku_find_square(x,y)
+                    i = sqr_coordinates[0]
+                    for i in range(i, i+3):
+                        j =sqr_coordinates[1]
+                        for j in range(j, j+3):
+                            if board_copy[i][j] != 0:
+                                if board_copy[i][j] in valid_numbers:
+                                    valid_numbers.remove(board_copy[i][j])
+                    try:
+                        board_copy[x][y] = random.choice(valid_numbers)
+                    except:
+                        board_copy[x][y] = random.randint(1,9)
+        if sudoku_checker(board_copy) == True:
+            print("Solution reached, number of attempts: {}".format(attempts))
+            return board_copy
+        else:
+            x = 0
+            board_copy = board.copy()
+            attempts += 1
+
